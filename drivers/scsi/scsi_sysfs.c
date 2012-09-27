5391617af5f827fb0a5764a4b0947a50c92b3691
franciscofranco <franciscofranco.1990@gmail.com>
2012-09-27 02:46:59 +0100

    3.0.40
    Conflicts:
    
    	Makefile
    	drivers/scsi/scsi_sysfs.c

--- drivers/scsi/scsi_sysfs.c
+++ drivers/scsi/scsi_sysfs.c
@@ -988,17 +988,26 @@ static void __scsi_remove_target(struct scsi_target *starget)
 void scsi_remove_target(struct device *dev)
 {
 	struct Scsi_Host *shost = dev_to_shost(dev->parent);
+<<<<<<< HEAD
 	struct scsi_target *starget, *last = NULL;
 	unsigned long flags;
 
 	/* remove targets being careful to lookup next entry before
 	 * deleting the last
 	 */
+=======
+	struct scsi_target *starget, *found;
+	unsigned long flags;
+
+ restart:
+	found = NULL;
+>>>>>>> bddfae8... 3.0.40
 	spin_lock_irqsave(shost->host_lock, flags);
 	list_for_each_entry(starget, &shost->__targets, siblings) {
 		if (starget->state == STARGET_DEL)
 			continue;
 		if (starget->dev.parent == dev || &starget->dev == dev) {
+<<<<<<< HEAD
 			/* assuming new targets arrive at the end */
 			starget->reap_ref++;
 			spin_unlock_irqrestore(shost->host_lock, flags);
@@ -1007,12 +1016,30 @@ void scsi_remove_target(struct device *dev)
 			last = starget;
 			__scsi_remove_target(starget);
 			spin_lock_irqsave(shost->host_lock, flags);
+=======
+			found = starget;
+			found->reap_ref++;
+			break;
+>>>>>>> bddfae8... 3.0.40
 		}
 	}
 	spin_unlock_irqrestore(shost->host_lock, flags);
 
+<<<<<<< HEAD
 	if (last)
 		scsi_target_reap(last);
+=======
+	if (found) {
+		__scsi_remove_target(found);
+		scsi_target_reap(found);
+		/* in the case where @dev has multiple starget children,
+		 * continue removing.
+		 *
+		 * FIXME: does such a case exist?
+		 */
+		goto restart;
+	}
+>>>>>>> bddfae8... 3.0.40
 }
 EXPORT_SYMBOL(scsi_remove_target);
 
