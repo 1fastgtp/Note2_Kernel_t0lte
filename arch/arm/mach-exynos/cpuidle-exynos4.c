4026fe2677c49bb3074584f5cf85fe2673f295be
Andrei F <luxneb@gmail.com>
2013-03-29 04:17:14 +0100

    cpuidle-exynos4: Different residency times for Note 2 users
    Instability is caused by too low residency times on Pega Prime, unidentified cause.
    
    Conflicts:
    
    	arch/arm/mach-exynos/cpuidle-exynos4.c
    
    Conflicts:
    
    	arch/arm/mach-exynos/cpuidle-exynos4.c

--- arch/arm/mach-exynos/cpuidle-exynos4.c
+++ arch/arm/mach-exynos/cpuidle-exynos4.c
@@ -766,6 +766,13 @@ static struct cpuidle_state exynos4_cpuidle_set[] = {
 		.enter			= exynos4_enter_lowpower,
 		.exit_latency		= 300,
 		.target_residency	= 1000,
+		
+#ifdef CONFIG_MACH_T0
+		.target_residency	= 7500,
+#else
+		.target_residency	= 1000,
+#endif
+
 		.flags			= CPUIDLE_FLAG_TIME_VALID,
 		.name			= "LOW_POWER",
 		.desc			= "ARM power down",
